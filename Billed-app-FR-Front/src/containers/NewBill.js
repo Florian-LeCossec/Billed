70c5577baee4e344f2fd0457b40c751624fd1374
Florian Le Cossec <aycet@MBP-de-Florian.lan>
2024-08-01 20:07:57 +0200

    Initial commit combining back and front directories

---
+++ Billed-app-FR-Front/src/containers/NewBill.js
@@ -0,0 +1,90 @@
+import { ROUTES_PATH } from '../constants/routes.js'
+import Logout from "./Logout.js"
+
+export default class NewBill {
+  constructor({ document, onNavigate, store, localStorage }) {
+    this.document = document
+    this.onNavigate = onNavigate
+    this.store = store
+    const formNewBill = this.document.querySelector(`form[data-testid="form-new-bill"]`)
+    formNewBill.addEventListener("submit", this.handleSubmit)
+    const file = this.document.querySelector(`input[data-testid="file"]`)
+    file.addEventListener("change", this.handleChangeFile)
+    this.fileUrl = null
+    this.fileName = null
+    this.billId = null
+    new Logout({ document, localStorage, onNavigate })
+    this.isValidImageType = false
+    this.formData = null
+  }
+  handleChangeFile = e => {
+    e.preventDefault()
+    const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
+    const filePath = e.target.value.split(/\\/g)
+    const fileName = filePath[filePath.length-1]
+    const formData = new FormData()
+    const email = JSON.parse(localStorage.getItem("user")).email
+    const validImageTypes = ['image/jpeg', 'image/png', 'image/jpg']
+    if(!validImageTypes.includes(file.type)) {
+      alert('Invalid file type')
+      return
+    }
+
+    this.isValidImageType = true
+    formData.append('file', file)
+    formData.append('email', email)
+    this.fileUrl = filePath
+    this.fileName = fileName
+    this.formData = formData
+  }
+
+  handleSubmit = e => {
+    e.preventDefault()
+    console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
+    const email = JSON.parse(localStorage.getItem("user")).email
+    const bill = {
+      email,
+      type: e.target.querySelector(`select[data-testid="expense-type"]`).value,
+      name:  e.target.querySelector(`input[data-testid="expense-name"]`).value,
+      amount: parseInt(e.target.querySelector(`input[data-testid="amount"]`).value),
+      date:  e.target.querySelector(`input[data-testid="datepicker"]`).value,
+      vat: e.target.querySelector(`input[data-testid="vat"]`).value,
+      pct: parseInt(e.target.querySelector(`input[data-testid="pct"]`).value) || 20,
+      commentary: e.target.querySelector(`textarea[data-testid="commentary"]`).value,
+      fileUrl: this.fileUrl,
+      fileName: this.fileName,
+      status: 'pending'
+    }
+
+    if(this.isValidImageType) {
+      this.store
+      .bills()
+      .create({
+        data: this.formData,
+        headers: {
+          noContentType: true
+        }
+      })
+      .then(({fileUrl, key}) => {
+        this.billId = key
+        this.fileUrl = fileUrl
+        this.fileName = this.fileName
+      }).then(() => {
+        this.updateBill(bill)
+      }).catch(error => console.error(error))
+    }
+  }
+
+  // not need to cover this function by tests
+  updateBill = (bill) => {
+    if (this.store) {
+      this.store
+      .bills()
+      .update({data: JSON.stringify(bill), selector: this.billId})
+      .then(() => {
+        this.onNavigate(ROUTES_PATH['Bills'])
+      })
+      .catch(error => console.error(error))
+    }
+  }
+}
